b081bbdb41d92174a7fd2aeeb7bd9a3e09eb7a7d
Suryansh Tiwari <suryansh2005tiwari@gmail.com>
2026-02-19 02:42:32 +0530

    Learn't about SureFire plugin it helps in loading a JVM to run all classes laigned with @Test or named as similar to *Test/*Tests/Test*

--- src/test/java/com/test/demo/ShapesTest.java
+++ src/test/java/com/test/demo/ShapesTest.java
@@ -5,12 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShapesTest {
+    Shapes shape = new Shapes();
 
     @Test
     void testcomputeSquareArea(){
-        Shapes shape = new Shapes();
-
         assertEquals(576,shape.computeSquareArea(24));
+    }
 
+    @Test
+    void testcomputeCircleArea(){
+        assertEquals(314,shape.computeCircleArea(10),"Area of Circle Calculation is right");
+        //string message is evaluated even if testcase  passes or fails
+    }
+
+    @Test
+    void testcomputeCircleAreaSupplier(){
+        assertEquals(314,shape.computeCircleArea(10),()->"Area of Circle Calculation is wrogn");
+        //string message is evaluated only if test case is failing
     }
 }
